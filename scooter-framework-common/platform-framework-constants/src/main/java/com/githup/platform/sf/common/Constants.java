7fd75dc84d5a6c31204382bb83250f0d376c85e4
xtawgipl <xtawgipl1123@163.com>
2020-01-21 13:46:13 +0800

    通用常量

---
+++ scooter-framework-common/platform-framework-constants/src/main/java/com/githup/platform/sf/common/Constants.java
@@ -0,0 +1,25 @@
+package com.githup.platform.sf.common;
+
+/**
+ * 常量
+ *
+ * @author zhangjj
+ * @create 2020-01-21 11:44
+ **/
+public class Constants {
+
+    /** 路径分割符号 */
+    public static final String PATH_SPLIT = "/";
+
+    /** 点分隔符 */
+    public static final String DOT_SPLIT = ".";
+
+
+    /** 占位符前缀 */
+    public static final String PLACEHOLDER_PREFIX = "${";
+
+
+    /** 占位符前缀 */
+    public static final String PLACEHOLDER_SUFFIX = "}";
+
+}
