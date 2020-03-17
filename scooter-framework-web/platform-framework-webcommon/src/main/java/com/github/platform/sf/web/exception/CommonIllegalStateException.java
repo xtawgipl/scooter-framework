fea3d6dd2753ce9d74e93177d27fd9a39e21b449
xtawgipl <xtawgipl1123@163.com>
2020-03-17 15:03:49 +0800

    commit

--- scooter-framework-web/platform-framework-webcommon/src/main/java/com/github/platform/sf/web/exception/CommonIllegalStateException.java
+++ scooter-framework-web/platform-framework-webcommon/src/main/java/com/github/platform/sf/web/exception/CommonIllegalStateException.java
@@ -5,7 +5,6 @@ package com.github.platform.sf.web.exception;
  * @author 张剑军
  * @Description
  * @date 2019/1/8 11:45
- * @Copyright: ©China software and Technical service Co.Ltd
  **/
 public class CommonIllegalStateException extends AppException {
 
